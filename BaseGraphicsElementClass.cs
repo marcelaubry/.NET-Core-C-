690d967ae40b68f4cc3a82debfdefe3738022bf1
marcelaubry <52050560+marcelaubry@users.noreply.github.com>
2021-01-16 20:35:17 -0500

    Add files via upload
    GUI that allows users to draw different shapes with different outline sizes/colors and fillings.

---
+++ BaseGraphicsElementClass.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class BaseGraphicsElementClass
+    {
+        public virtual void Draw(Graphics g) { }
+
+        public Color SelectedColor(int nonConvertedColor)
+        {
+            Color convertedColor;
+            if (nonConvertedColor == 1)
+            {
+                convertedColor = Color.Black;
+                return convertedColor;
+            }
+            else if (nonConvertedColor == 2)
+            {
+                convertedColor = Color.Red;
+                return convertedColor;
+            }
+            else if (nonConvertedColor == 3)
+            {
+                convertedColor = Color.Blue;
+                return convertedColor;
+            }
+            else if (nonConvertedColor == 4)
+            {
+                convertedColor = Color.Green;
+                return convertedColor;
+            }
+            else { return Color.Empty; }
+        }
+    }
+    class LineClass : BaseGraphicsElementClass
+    {
+        private readonly Point Point1, Point2;
+        private readonly Color PenColor;
+        private readonly int PenSize;
+
+        public LineClass(Point inputPoint1, Point inputPoint2, int inputPenColor, int inputPenSize)
+        {
+            Point1 = inputPoint1;
+            Point2 = inputPoint2;
+            PenColor = SelectedColor(inputPenColor);
+            PenSize = inputPenSize;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            var myPen = new Pen(PenColor, PenSize);
+            g.DrawLine(myPen, Point1, Point2);
+        }
+    }
+
+    class RectangleClass : BaseGraphicsElementClass
+    {
+        private readonly int PenSize, X, Y, Width, Height;
+        private readonly Color Outline, FillColor;
+
+        public RectangleClass(Point inputPoint1, Point inputPoint2, int inputPenColor, int inputPenFill,
+                              int inputPenSize)
+        {
+            X = inputPoint1.X > inputPoint2.X ? inputPoint2.X : inputPoint1.X;
+            Y = inputPoint1.Y > inputPoint2.Y ? inputPoint2.Y : inputPoint1.Y;
+            Width = Math.Abs(inputPoint1.X - inputPoint2.X);
+            Height = Math.Abs(inputPoint1.Y - inputPoint2.Y);
+            Outline = SelectedColor(inputPenColor);
+            FillColor = SelectedColor(inputPenFill);
+            PenSize = inputPenSize;
+        }
+        public override void Draw(Graphics g)
+        {
+            var myOutlinePen = new Pen(Outline, PenSize);
+            using (Brush myFillBrush = new SolidBrush(FillColor))
+            {
+                // Color.Empty was assigned if no fill or no outline was selected respectively
+                if (FillColor != Color.Empty)
+                {
+                    g.FillRectangle(myFillBrush, X, Y, Width, Height);
+                }
+                if (Outline != Color.Empty)
+                {
+                    g.DrawRectangle(myOutlinePen, X, Y, Width, Height);
+                }
+            }
+        }
+    }
+
+    class EllipseClass : BaseGraphicsElementClass
+    {
+        private readonly int PenSize, X, Y, Width, Height;
+        private readonly Color Outline, FillColor;
+
+        public EllipseClass(Point inputPoint1, Point inputPoint2, int inputPenColor, int inputPenFill, 
+                            int inputPenSize)
+        {
+            X = inputPoint1.X > inputPoint2.X ? inputPoint2.X : inputPoint1.X;
+            Y = inputPoint1.Y > inputPoint2.Y ? inputPoint2.Y : inputPoint1.Y;
+            Width = Math.Abs(inputPoint1.X - inputPoint2.X);
+            Height = Math.Abs(inputPoint1.Y - inputPoint2.Y);
+            Outline = SelectedColor(inputPenColor);
+            FillColor = SelectedColor(inputPenFill);
+            PenSize = inputPenSize;
+        }
+        public override void Draw(Graphics g)
+        {
+            var myOutlinePen = new Pen(Outline, PenSize);
+            using (Brush myFillBrush = new SolidBrush(FillColor))
+            {
+                // Color.Empty was assigned if no fill or no outline was selected respectively
+                if (FillColor != Color.Empty)
+                {
+                    g.FillEllipse(myFillBrush, X, Y, Width, Height);
+                }
+                if (Outline != Color.Empty)
+                {
+                    g.DrawEllipse(myOutlinePen, X, Y, Width, Height);
+                }  
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class settingsForm : Form
+    {
+        private int Outline;
+        private int FillColor;
+        private int PenWidth;
+
+        // Assign list box values
+        public settingsForm(int inputOutline, int inputFillColor, int inputPenWidth)
+        {
+            InitializeComponent();
+            Outline = inputOutline;
+            listBoxOutline.SelectedIndex = Outline;
+            FillColor = inputFillColor;
+            listBoxFillColor.SelectedIndex = FillColor;
+            PenWidth = inputPenWidth;
+            listBoxPenWidth.SelectedIndex = PenWidth;
+        }
+        private void CancelSettingsButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            Outline = listBoxOutline.SelectedIndex;
+            FillColor = listBoxFillColor.SelectedIndex;
+            PenWidth = listBoxPenWidth.SelectedIndex;
+        }
+
+        public int GetOutline()
+        {
+            return Outline;
+        }
+        public int GetFillColor()
+        {
+            return FillColor;
+        }
+        public int GetPenWidth()
+        {
+            return PenWidth;
+        }
+    }
+}
